c57a1eed5f2735ef5329d3d49189116762af9be8
andtamas <andrei.gabriel.tamas@stud.ubbcluj.ro>
2025-10-27 20:11:31 +0200

    library und member repository hinzugefuegt

---
+++ src/main/java/com/example/repository/LibraryRepository.java
@@ -0,0 +1,46 @@
+package com.example.repository;
+
+import com.example.model.Library;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryRepository {
+    private List<Library> libraryList;
+
+    public LibraryRepository() {
+        libraryList = new ArrayList<Library>();
+    }
+
+    public void save(Library library) {
+        for (int i = 0; i < libraryList.size(); i++) {
+            if (libraryList.get(i).getId().equals(library.getId())) {
+                libraryList.set(i, library);
+                return;
+            }
+        }
+        libraryList.add(library);
+    }
+
+    public List<Library> getLibraryList() {
+        return libraryList;
+    }
+
+    public Library getLibraryById(String id) {
+        for (int i = 0; i < libraryList.size(); i++) {
+            if (libraryList.get(i).getId().equals(id)) {
+                return libraryList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public boolean delete(String id) {
+        for (int i = 0; i < libraryList.size(); i++) {
+            if (libraryList.get(i).getId().equals(id)) {
+                libraryList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -0,0 +1,46 @@
+package com.example.repository;
+
+import com.example.model.Member;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberRepository {
+    private List<Member> memberList;
+
+    public MemberRepository() {
+        memberList = new ArrayList<Member>();
+    }
+
+    public void save(Member member) {
+        for (int i = 0; i < memberList.size(); i++) {
+            if (memberList.get(i).getId().equals(member.getId())) {
+                memberList.set(i, member);
+                return;
+            }
+        }
+        memberList.add(member);
+    }
+
+    public List<Member> getMemberList() {
+        return memberList;
+    }
+
+    public Member getMemberById(String id) {
+        for (int i = 0; i < memberList.size(); i++) {
+            if (memberList.get(i).getId().equals(id)) {
+                return memberList.get(i);
+            }
+        }
+        return null;
+    }
+
+    public boolean delete (Member member) {
+        for (int i = 0; i < memberList.size(); i++) {
+            if (memberList.get(i).getId().equals(member.getId())) {
+                memberList.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
